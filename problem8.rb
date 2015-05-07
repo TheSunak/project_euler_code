6bc22d1087a62826b47ae7c207090fc6aa6843ae
Vishal Sunak <vishal@backupify.com>
2015-05-07 13:57:23 -0400

    Problem 8 tweak to remove empty lines.

--- problem8.rb
+++ problem8.rb
@@ -49,11 +49,9 @@ num = [7,3,1,6,7,1,7,6,5,3,1,3,3,0,6,2,4,9,1,9,2,2,5,1,1,9,6,7,4,4,2,6,5,7,4,7,4
 0,5,8,8,6,1,1,6,4,6,7,1,0,9,4,0,5,0,7,7,5,4,1,0,0,2,2,5,6,9,8,3,1,5,5,2,0,0,0,5,5,9,3,5,7,2,9,7,2,5,
 7,1,6,3,6,2,6,9,5,6,1,8,8,2,6,7,0,4,2,8,2,5,2,4,8,3,6,0,0,8,2,3,2,5,7,5,3,0,4,2,0,7,5,2,9,6,3,4,5,0]
 
-
 index = 0
 local_max = 0
 max_product = 0
-index_of_max = 0
 
 def product_of_thirteen(array,index)
 
@@ -78,7 +76,6 @@ while index < 987
   
   if local_max > max_product 
     max_product = local_max
-    index_of_max = index
   end
 
   index = index + 1
@@ -86,7 +83,3 @@ while index < 987
 end
 
 puts max_product
-
-
-
-
